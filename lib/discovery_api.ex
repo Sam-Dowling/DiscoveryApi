c2cda6546c39302c4d43113b9867c7420a227777
Sam Dowling <sam.dowling@students.ittralee.ie>
2017-05-28 15:38:38 +0100

    Mix generated code with Supervisor support

---
+++ lib/discovery_api.ex
@@ -0,0 +1,18 @@
+defmodule DiscoveryApi do
+  @moduledoc """
+  Documentation for DiscoveryApi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DiscoveryApi.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/discovery_api_test.exs
@@ -0,0 +1,8 @@
+defmodule DiscoveryApiTest do
+  use ExUnit.Case
+  doctest DiscoveryApi
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
