ad97e65be00537fbb83f60875e941d99dff5f951
Sam Dowling <sam.dowling@students.ittralee.ie>
2017-05-30 17:00:11 +0100

    Added commented Routes for new region functiality.

--- web/router.ex
+++ web/router.ex
@@ -11,5 +11,11 @@ defmodule DiscoveryApi.Router do
     get "/services", ServiceController, :index
     get "/services/:id", ServiceController, :show
     post "/services/:id", ServiceController, :create
+
+    # get "/services", ServiceController, :regions # gets a list of regions
+    # get "/services/:reg", ServiceController, :index # gets a list of services from a region
+    # get "/services/:reg/:id", ServiceController, :show # gets an instance of a service (:id) from a region (:reg)
+    #
+    # post "/services/:reg/:id", ServiceController, :create # registers an instance of service (:id) for region (:reg)
   end
 end
